32a26d89caac3ee5231ac72cf6d4309fa5fe9288
Aaron Parecki <aaron@parecki.com>
2013-11-16 22:34:09 -0800

    more sane debugging output

--- bid.js
+++ bid.js
@@ -16,8 +16,12 @@ sub.on('message', function(channel, message) {
   // Only respond to standard "privmsg" messages (includes channel and private messages)
   if(msg.version == 1 && msg.type == "privmsg") {
     if(match=msg.data.message.match(/^[\/!]bid ([^@]+) (@.+)/i)) {
+
+      console.log(sender+' > '+msg.data.channel+' :: '+msg.data.message);
+      console.log("\t\t(starting a new bid)");
+
       // Match the proper '!bid' or '/bid' command
-      console.log(match);
+      //console.log(match);
 
       var description = match[1].replace(/ with$/, '');
       var people = match[2].match(/@?[a-z]+/gi);
@@ -33,21 +37,24 @@ sub.on('message', function(channel, message) {
       }));
       redis.del('current-bid-values');
 
-      console.log({
-        description: description,
-        people: people
-      });
+      // console.log({
+      //   description: description,
+      //   people: people
+      // });
 
       for(var i=0; i<people.length; i++) {
         (function(msg, sender, description, people, person){
           // Look up the ID to send a private message to each user
           pmid_for_nick(person, function(err, pmid){
             if(pmid) {
+              var message;
               if(sender.replace(/^@/,'') == person.replace(/^@/,'')) {
-                zen.send_privmsg(pmid, 'Tell me your bid by replying with "!bid 20"');
+                message = 'Tell me your bid by replying with "!bid 20"';
               } else {
-                zen.send_privmsg(pmid, sender+' is requesting bids '+description+'. Reply with "!bid 20"');
+                message = sender+' is requesting bids '+description+'. Reply with "!bid 20"';
               }
+              console.log('me > '+pmid+' :: '+message);
+              zen.send_privmsg(pmid, message);
             }
           })
 
@@ -57,23 +64,28 @@ sub.on('message', function(channel, message) {
       zen.send_privmsg(msg.data.channel, 'Ok, I\'ll collect bids from them');
 
     } else if(match=msg.data.message.match(/^[\/!]bid ([0-9\.]+)$/)) {
-      console.log("Received a bid: " + match[1]);
+      console.log(sender+' > '+msg.data.channel+' :: '+msg.data.message);
+      console.log("\t\t(accepting a bid)");
 
       // Reject if no bid in progress
       redis.get('current-bid', function(err, current_bid){
         if(current_bid == null) {
-          zen.send_privmsg(msg.data.channel, 'Sorry, there is no bid in progress. Try: !bid for buying groceries with @bee');
+          var message = 'Sorry, there is no bid in progress. Try: !bid for buying groceries with @bee';
+          console.log('me > '+msg.data.channel+' :: '+message);
+          zen.send_privmsg(msg.data.channel, message);
         } else {
           // Store this person's bid
           redis.hset('current-bid-values', sender.replace(/^@/,''), match[1]);
 
-          zen.send_privmsg(msg.data.channel, 'Got it! I\'ll report back when I\'ve collected everyone\'s bids.');
+          var message = 'Got it! I\'ll report back when I\'ve collected everyone\'s bids.';
+          console.log('me > '+msg.data.channel+' :: '+message);
+          zen.send_privmsg(msg.data.channel, message);
 
           // Check if we have collected all the bids
           (function(current_bid){
             redis.hgetall('current-bid-values', function(err, bid_data){
-              console.log("Current bid:", current_bid);
-              console.log("Bid data:", bid_data);
+              // console.log("Current bid:", current_bid);
+              // console.log("Bid data:", bid_data);
 
               var complete = true;
               for(var i=0; i<current_bid.people.length; i++) {
@@ -81,7 +93,7 @@ sub.on('message', function(channel, message) {
                   complete = false;
                 }
               }
-              console.log("Bid complete? " + (complete ? "y" : "n"));
+              // console.log("Bid complete? " + (complete ? "y" : "n"));
 
               if(complete) {
                 // Output all bids back to the main channel
@@ -89,7 +101,9 @@ sub.on('message', function(channel, message) {
                 for(var i=0; i<current_bid.people.length; i++) {
                   bid_strings.push(current_bid.people[i] + ": " + bid_data[current_bid.people[i].replace(/^@/,'')]);
                 }
-                zen.send_privmsg(current_bid.channel, "Bidding complete! Here are the bids: "+bid_strings.join(', '));
+                var message = "Bidding complete! Here are the bids: "+bid_strings.join(', ');
+                console.log('me > '+current_bid.channel+' :: '+message);
+                zen.send_privmsg(current_bid.channel, message);
                 redis.del('current-bid');
                 redis.del('current-bid-values');
               }
@@ -102,21 +116,27 @@ sub.on('message', function(channel, message) {
 
       redis.get('current-bid', function(err, current_bid){
         if(current_bid == null) {
-          zen.send_privmsg(msg.data.channel, 'There is no bid in progress.');
+          var message = 'There is no bid in progress.';
+          console.log('me > '+msg.data.channel+' :: '+message);
+          zen.send_privmsg(msg.data.channel, message);
         } else {
           redis.hgetall('current-bid-values', function(err, bid_data){
             var names = [];
             for(var name in bid_data) {
               names.push(name);
             }
-            zen.send_privmsg(msg.data.channel, 'So far I have collected bids from: '+names.join(', '));
+            var message = 'So far I have collected bids from: '+names.join(', ');
+            console.log('me > '+msg.data.channel+' :: '+message);
+            zen.send_privmsg(msg.data.channel, message);
           });
         }
       });
 
     } else if(match=msg.data.message.match(/^[\/!]bid [^@]+$/)) {
       // Catch some common errors, such as not mentioning people with an "@"
-      zen.send_privmsg(msg.data.channel, 'Sorry I didn\'t get that. Try: !bid for buying groceries with @bee');
+      var message = 'Sorry I didn\'t get that. Try: !bid for buying groceries with @bee';
+      console.log('me > '+msg.data.channel+' :: '+message);
+      zen.send_privmsg(msg.data.channel, message);
     }
   }
 });
